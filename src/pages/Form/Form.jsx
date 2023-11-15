7ecb9aac4b137a467188a939e9a28e6d794c27c6
Ganesh <ghattiganesh8@gmail.com>
2023-11-15 19:44:41 +0530

    first

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -50,14 +50,18 @@ export default function Form() {
           className="w-1/2 flex justify-center flex-col gap-5 md:w-full"
           style={{ padding: "0 6vw" }}
         >
-          <h1 className="text-6xl font-extrabold" style={{ color: "#32355D" }}>
-            Start your own &nbsp;
-            <br className="lg:hidden" />
+          <h1 className="text-5xl font-extrabold" style={{ color: "#32355D" }}>
+            Start your own
+            <span className="md:hidden">&nbsp;</span>
+            <br className="xl:hidden lg:hidden md:hidden" />
             <span className="" style={{ color: "#7F46FF" }}>
               Preschool
             </span>
           </h1>
-          <p style={{ color: "#0B1C39" }} className="text-2xl font-normal">
+          <p
+            style={{ color: "#0B1C39" }}
+            className="text-2xl font-normal md:text-xl"
+          >
             <span className="font-extrabold">
               Become the #1 in your area. Just fill out the form and we will get
               you started.
@@ -143,10 +147,10 @@ export default function Form() {
           >
             Enquire Now!
           </Button>
-          <p className="font-normal text-base">
+          <p className="font-normal text-base md:text-center">
             Your email address and mobile number will be safely managed
             according to our &nbsp;
-            <a href="#" style={{ color: "#1682FB" }} className="font-bold">
+            <a href="#" style={{ color: "#1682FB" }} className="font-bold unerline">
               Privacy Policy
             </a>
           </p>
@@ -155,11 +159,11 @@ export default function Form() {
 
       <section
         id="form-numbers"
-        className="w-full flex flex-col items-center justify-center gap-16 py-16"
+        className="w-full flex flex-col items-center justify-center gap-16 py-16 px-12 "
       >
         <h1
-          className="text-6xl font-extrabold md:text-center"
-          style={{ color: "#32355D" }}
+          className="text-5xl font-extrabold md:text-center "
+          style={{ color: "#32355D", padding: "0 15vw" }}
         >
           Endurance & Success
         </h1>
@@ -172,14 +176,14 @@ export default function Form() {
               Years of Expertise
             </p>
             <p
-              className="text-7xl font-extrabold sm:text-5xl"
+              className="text-7xl lg:text-6xl font-extrabold sm:text-5xl"
               style={{ color: "#F72378" }}
             >
               <span className="opacity-20">0</span>16+
             </p>
           </div>
           <div
-            className="h-44 w-0.5 lg:hidden"
+            className="h-44 w-0.5 lg:hidden md:hidden"
             style={{ backgroundColor: "#0B1C39" }}
           ></div>
           <div className="w-1/6 px-2 flex flex-col justify-around md:w-1/2  md:flex-col-reverse md:items-center">
@@ -190,14 +194,14 @@ export default function Form() {
               School Set-ups
             </p>
             <p
-              className="text-7xl font-extrabold sm:text-5xl"
+              className="text-7xl lg:text-6xl font-extrabold sm:text-5xl"
               style={{ color: "#F72378" }}
             >
               <span className="opacity-20 ">0</span>200+
             </p>
           </div>
           <div
-            className="h-44 w-0.5 lg:hidden"
+            className="h-44 w-0.5 lg:hidden md:hidden"
             style={{ backgroundColor: "#0B1C39" }}
           ></div>
 
@@ -209,14 +213,14 @@ export default function Form() {
               Children Developed
             </p>
             <p
-              className="text-7xl font-extrabold sm:text-5xl"
+              className="text-7xl lg:text-6xl font-extrabold sm:text-5xl"
               style={{ color: "#F72378" }}
             >
               35000+
             </p>
           </div>
           <div
-            className="h-44 w-0.5 lg:hidden"
+            className="h-44 w-0.5 lg:hidden md:hidden"
             style={{ backgroundColor: "#0B1C39" }}
           ></div>
 
@@ -228,16 +232,161 @@ export default function Form() {
               Cities
             </p>
             <p
-              className="text-7xl font-extrabold sm:text-5xl"
+              className="text-7xl lg:text-6xl font-extrabold sm:text-5xl"
               style={{ color: "#F72378" }}
             >
               <span className="opacity-20">0</span>30+
             </p>
           </div>
         </div>
-        <p className="text-3xl font-bold md:text-xl" style={{ color: "#0B1C39" }}>
+        <p
+          className="text-3xl font-bold md:text-xl"
+          style={{ color: "#0B1C39" }}
+        >
           and growing...
         </p>
+      </section>
+
+      <section
+        id="form-highlights"
+        className="flex w-11/12 justify-around items-center md:flex-col py-16 lg:flex-wrap md:gap-8"
+      >
+        <h1
+          className="text-5xl font-extrabold w-1/5 md:text-center  lg:w-2/5 md:w-full"
+          style={{ color: "#32355D" }}
+        >
+          Our Pricing
+          <br /> Highlights
+        </h1>
+        <div
+          className="w-1/5 p-8 h-72 flex flex-col justify-center gap-4 lg:w-2/5 md:w-full"
+          style={{
+            backgroundColor: "#D0E6FE",
+            borderRadius: "24px",
+            border: " 1px solid  #1682FB",
+          }}
+        >
+          <p className="text-2xl font-normal">New Preschool Packages:</p>
+          <p
+            className="text-3xl font-normal 2xl:text-2xl"
+            style={{ color: "#0B1C39" }}
+          >
+            Starts at
+            <br />
+            <span className="font-bold" style={{ color: "#32355D" }}>
+              ₹3.50 Lakhs only
+            </span>
+          </p>
+          <p
+            className="flex font-bold text-xl underline items-center"
+            style={{ color: "#F72378" }}
+          >
+            Let’s Discuss
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="24"
+              viewBox="0 0 25 24"
+              fill="none"
+            >
+              <path
+                d="M3.6665 12H21.6665M21.6665 12L15.6665 6M21.6665 12L15.6665 18"
+                stroke="#F72378"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </p>
+        </div>
+
+        <div
+          className="w-1/5 p-8 h-72 flex flex-col justify-center gap-4 lg:w-2/5 md:w-full"
+          style={{
+            backgroundColor: "#FEFDDE ",
+            borderRadius: "24px",
+            border: "1px solid #FBF55A",
+          }}
+        >
+          <p className="text-2xl font-normal">
+            Existing Preschool Support Packages:
+          </p>
+          <p
+            className="text-3xl font-normal 2xl:text-2xl"
+            style={{ color: "#0B1C39" }}
+          >
+            Starts at
+            <br />
+            <span className="font-bold" style={{ color: "#32355D" }}>
+              ₹20,000 p.a only
+            </span>
+          </p>
+          <p
+            className="flex font-bold text-xl underline items-center"
+            style={{ color: "#F72378" }}
+          >
+            Let’s Discuss
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="24"
+              viewBox="0 0 25 24"
+              fill="none"
+            >
+              <path
+                d="M3.6665 12H21.6665M21.6665 12L15.6665 6M21.6665 12L15.6665 18"
+                stroke="#F72378"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </p>
+        </div>
+
+        <div
+          className="w-1/5 p-8 h-72 flex flex-col justify-center gap-4 lg:w-2/5 md:w-full"
+          style={{
+            backgroundColor: "#E5DAFF",
+            borderRadius: "24px",
+            border: " 1px solid  #7F46FF",
+          }}
+        >
+          <p className="text-2xl font-normal">
+            Dedicated B2B Purchasing portal:
+          </p>
+          <p
+            className="text-3xl font-bold 2xl:text-2xl"
+            style={{ color: "#0B1C39" }}
+          >
+            Most Economical
+            <br />
+            <span className="font-normal" style={{ color: "#32355D" }}>
+              for Resource
+            </span>
+          </p>
+          <p
+            className="flex font-bold text-xl underline items-center"
+            style={{ color: "#F72378" }}
+          >
+            Let’s Discuss
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="24"
+              viewBox="0 0 25 24"
+              fill="none"
+            >
+              <path
+                d="M3.6665 12H21.6665M21.6665 12L15.6665 6M21.6665 12L15.6665 18"
+                stroke="#F72378"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </p>
+        </div>
       </section>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,10 @@ export default {
     extend: {
       screens: {
         sm: { max: "639px" },
-        md: { max: "867px" },
-        lg: { max: "1024px" },
-        xl: { max: "1279px" },
-        "2xl": { max: "1535px" },
+        md: {  max: "866px" }, // Adjusted min and max values for md
+        lg: { min: "867px", max: "1050px" }, // Adjusted min and max values for lg
+        xl: { min: "1051px", max: "1279px" }, // Adjusted min and max values for xl
+        "2xl": { min: "1280px", max: "1335px" }, // Adjusted min and max values for 2xl
       },
     },
   },
