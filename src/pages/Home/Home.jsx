98d8e314825feac6cba776cbc5fc7683d57155de
Ganesh <ghattiganesh8@gmail.com>
2023-11-15 16:05:22 +0530

    first

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 import "./Home.scss";
+import { Link } from "react-router-dom";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <div>
       home
+      <Link to="/form">
+        <Button color="primary">Form Page</Button>
+      </Link>
     </div>
-  )
+  );
 }
