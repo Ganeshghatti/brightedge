bd7e0d729f3616d12b771a4fb741ff56ba44d77d
Ganesh <ghattiganesh8@gmail.com>
2023-11-16 00:27:52 +0530

    first

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -409,35 +409,217 @@ export default function Form() {
         />
         <div className="flex justify-around items-center md:flex-col">
           <div className="flex flex-col justify-center w-1/4 md:w-11/12">
-            <p style={{ color: "#FF833F" }} className="text-4xl font-extrabold text-left md:text-3xl">
+            <p
+              style={{ color: "#FF833F" }}
+              className="text-4xl font-extrabold text-left md:text-3xl"
+            >
               Not an average franchise program.
             </p>
-            <p style={{ color: "#0B1C39" }} className="text-xl font-normal md:text-lg">
+            <p
+              style={{ color: "#0B1C39" }}
+              className="text-xl font-normal md:text-lg"
+            >
               Watch the video for on overview.
             </p>
           </div>
           <ul className="flex flex-col w-1/2 list-disc gap-4 md:w-3/4 md:mt-6">
             <li className="text-xl md:text-lg">
-              <span className="font-bold" style={{color:"#FF833F"}}>Revolutionary Model:</span>Redefining the franchise program
-              with a groundbreaking flexible approach.
+              <span className="font-bold" style={{ color: "#FF833F" }}>
+                Revolutionary Model:
+              </span>
+              Redefining the franchise program with a groundbreaking flexible
+              approach.
             </li>
             <li className="text-xl md:text-lg">
-              <span className="font-bold" style={{color:"#FF833F"}}>Zero Royalties:</span> Keep all your hard-earned profits
-              without paying any royalties.
+              <span className="font-bold" style={{ color: "#FF833F" }}>
+                Zero Royalties:
+              </span>
+              Keep all your hard-earned profits without paying any royalties.
             </li>
             <li className="text-xl md:text-lg">
-              <span className="font-bold" style={{color:"#FF833F"}}>Franchise Freedom:</span>Say goodbye to franchise fees and
-              exit fees, putting you in control.
+              <span className="font-bold" style={{ color: "#FF833F" }}>
+                Franchise Freedom:
+              </span>
+              Say goodbye to franchise fees and exit fees, putting you in
+              control.
             </li>
             <li className="text-xl md:text-lg">
-              <span className="font-bold" style={{color:"#FF833F"}}>Simplicity Matters:</span> A straightforward agreement that
-              makes business. ownership easier.
+              <span className="font-bold" style={{ color: "#FF833F" }}>
+                Simplicity Matters:
+              </span>
+              A straightforward agreement that makes business. ownership easier.
             </li>
             <li className="text-xl md:text-lg">
-              <span className="font-bold" style={{color:"#FF833F"}}>Welcome to the Affiliate Program:</span> A new era in
-              franchising awaits.
+              <span className="font-bold" style={{ color: "#FF833F" }}>
+                Welcome to the Affiliate Program:
+              </span>
+              A new era in franchising awaits.
             </li>
           </ul>
+        </div>
+      </section>
+
+      <section
+        id="form-hereisallyouneed"
+        className="py-16 gap-12 w-full flex flex-col items-center"
+      >
+        <h1
+          className="text-5xl font-extrabold md:text-center"
+          style={{ color: "#32355D", padding: "0 15vw" }}
+        >
+          Here’s all you need!
+        </h1>
+
+        <div className="flex justify-around w-full items-center lg:flex-wrap md:flex-col">
+          <div className="form-hereisallyouneed-card1 flex flex-col justify-around px-10 h-64 w-1/5 lg:w-2/5 md:w-11/12 lg:mt-6 md:mt-6">
+            <p style={{ color: "#7F46FF" }} className="text-4xl font-bold">
+              Investment:
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+            >
+              <rect width="48" height="48" fill="white" />
+              <path
+                d="M24 42C33.9411 42 42 33.9411 42 24C42 14.0589 33.9411 6 24 6C14.0589 6 6 14.0589 6 24C6 33.9411 14.0589 42 24 42Z"
+                fill="white"
+                stroke="#7F46FF"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M24 42C33.9411 42 42 33.9411 42 24C42 14.0589 33.9411 6 24 6C14.0589 6 6 14.0589 6 24C6 33.9411 14.0589 42 24 42Z"
+                fill="white"
+                stroke="#7F46FF"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M42 24C42 33.9411 33.9411 42 24 42C14.0589 42 6 33.9411 6 24C6 14.0589 14.0589 6 24 6C33.9411 6 42 14.0589 42 24Z"
+                fill="white"
+                stroke="#7F46FF"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M28 16H22C19.7909 16 18 17.7909 18 20C18 22.2091 19.7909 24 22 24H26C28.2091 24 30 25.7909 30 28C30 30.2091 28.2091 32 26 32L19.9998 31.9999"
+                stroke="#7F46FF"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M24 16V14M24 32V34"
+                stroke="#7F46FF"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <p className="text-2xl font-normal">
+              ₹4 Lakhs - ₹10
+              <br /> Lakhs
+            </p>
+          </div>
+
+          <div className="form-hereisallyouneed-card2 flex flex-col justify-around px-10 h-64 w-1/5 lg:w-2/5 md:w-11/12 lg:mt-6 md:mt-6">
+            <p style={{ color: "#1682FB" }} className="text-4xl font-bold">
+              Space:
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+            >
+              <path
+                d="M8 24H24M24 24H40M24 24V40M24 24L24 8"
+                stroke="#1682FB"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-dasharray="0.2 8"
+              />
+              <path
+                d="M38 8H10C8.89543 8 8 8.89543 8 10V38C8 39.1046 8.89543 40 10 40H38C39.1046 40 40 39.1046 40 38V10C40 8.89543 39.1046 8 38 8Z"
+                fill="white"
+                stroke="#1682FB"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <p className="text-2xl font-normal">About 1200 Sq.ft and above</p>
+          </div>
+
+          <div className="form-hereisallyouneed-card3 flex flex-col justify-around px-10 h-64 w-1/5 lg:w-2/5 md:w-11/12 lg:mt-6 md:mt-6">
+            <p style={{ color: "#F72378" }} className="text-4xl font-bold">
+              Passion:
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+            >
+              <path
+                d="M7.50984 26.6019C6.55212 24.9446 6 22.9952 6 20.9091C6 14.8842 10.6051 10 16.2857 10C19.3578 10 22.1153 11.4284 24 13.6932C25.8847 11.4284 28.6422 10 31.7143 10C37.3949 10 42 14.8842 42 20.9091C42 22.9952 41.4479 24.9446 40.4902 26.6019C36.2171 34.5497 24 40 24 40C24 40 11.7829 34.5497 7.50984 26.6019Z"
+                stroke="#F72378"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <p className="text-2xl font-normal">
+              For early childhood education
+            </p>
+          </div>
+          <div className="form-hereisallyouneed-card4 flex flex-col justify-around px-10 h-64 w-1/5 lg:w-2/5 md:w-11/12 lg:mt-6 md:mt-6">
+            <p style={{ color: "#7F46FF" }} className="text-4xl font-bold">
+              Focus:
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+              fill="none"
+            >
+              <rect width="48" height="48" fill="white" />
+              <path
+                d="M38 24H42M24 38V42M10 24H6M24 10V6"
+                stroke="#FF833F"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M38 24C38 31.732 31.732 38 24 38C16.268 38 10 31.732 10 24C10 16.268 16.268 10 24 10C31.732 10 38 16.268 38 24Z"
+                fill="white"
+                stroke="#FF833F"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M30 24C30 27.3137 27.3137 30 24 30C20.6863 30 18 27.3137 18 24C18 20.6863 20.6863 18 24 18C27.3137 18 30 20.6863 30 24Z"
+                fill="white"
+                stroke="#FF833F"
+                stroke-width="4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <p className="text-2xl font-normal">For entrepreneurship</p>
+          </div>
         </div>
       </section>
     </div>
