fc8e03af3029ec375db6c0edcc37c13c7ab646a8
Ganesh <ghattiganesh8@gmail.com>
2023-11-30 11:47:01 +0530

    added responsive carousel arrows

--- src/Components/CustomLeftArrow.jsx
+++ src/Components/CustomLeftArrow.jsx
@@ -7,33 +7,64 @@ const CustomLeftArrow = ({ onClick }) => {
       className={`custom-arrow custom-left-arrow absolute left-5 top-1/2`}
     >
       {/* <ArrowLeftIcon style={{ color: "white" }} /> */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="51"
-        height="51"
-        viewBox="0 0 51 51"
-        fill="none"
-      >
-        <circle
-          cx="25.4438"
-          cy="25.4438"
-          r="25.4438"
-          transform="matrix(-1 0 0 1 50.8877 0.112427)"
-          fill="#1682FB"
-        />
-        <path
-          d="M27.4585 34.7568L18.5308 25.8038L27.4585 16.8509"
-          fill="#1682FB"
-        />
-        <path
-          d="M27.4585 34.7568L18.5308 25.8038L27.4585 16.8509"
-          stroke="white"
-          strokeWidth={2.96331}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </button>)
+      {window.innerWidth > 867 ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="51"
+          height="51"
+          viewBox="0 0 51 51"
+          fill="none"
+        >
+          <circle
+            cx="25.4438"
+            cy="25.4438"
+            r="25.4438"
+            transform="matrix(-1 0 0 1 50.8877 0.112427)"
+            fill="#1682FB"
+          />
+          <path
+            d="M27.4585 34.7568L18.5308 25.8038L27.4585 16.8509"
+            fill="#1682FB"
+          />
+          <path
+            d="M27.4585 34.7568L18.5308 25.8038L27.4585 16.8509"
+            stroke="white"
+            strokeWidth={2.96331}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+        >
+          <ellipse
+            cx="12.4999"
+            cy="12.5"
+            rx="12.4999"
+            ry="12.5"
+            transform="matrix(-1 0 0 1 25 0)"
+            fill="#1682FB"
+          />
+          <path
+            d="M13.4897 17.0201L9.1038 12.6216L13.4897 8.22325"
+            fill="#1682FB"
+          />
+          <path
+            d="M13.4897 17.0201L9.1038 12.6216L13.4897 8.22325"
+            stroke="white"
+            stroke-width="2.96331"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )}
+    </button>
+  );
 };
 
 export default CustomLeftArrow;
--- src/Components/CustomRightArrow.jsx
+++ src/Components/CustomRightArrow.jsx
@@ -1,30 +1,49 @@
 import React from "react";
 
 const CustomRightArrow = ({ onClick }) => {
-  return  (
+  return (
     <button
       onClick={onClick}
       className={`custom-arrow custom-right-arrow absolute right-5 top-1/2`}
     >
       {/* <ArrowRightIcon style={{ color: "white" }} /> */}
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="51"
-        height="51"
-        viewBox="0 0 51 51"
-        fill="none"
-      >
-        <circle cx="25.5561" cy="25.5562" r="25.4438" fill="#1682FB" />
-        <path
-          d="M23.5415 34.7568L32.4692 25.8038L23.5415 16.8509"
-          stroke="white"
-          strokeWidth="2.97588"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      {window.innerWidth > 867 ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="51"
+          height="51"
+          viewBox="0 0 51 51"
+          fill="none"
+        >
+          <circle cx="25.5561" cy="25.5562" r="25.4438" fill="#1682FB" />
+          <path
+            d="M23.5415 34.7568L32.4692 25.8038L23.5415 16.8509"
+            stroke="white"
+            strokeWidth="2.97588"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+        >
+          <circle cx="12.5" cy="12.5" r="12.5" fill="#1682FB" />
+          <path
+            d="M11.5103 17.0201L15.8962 12.6216L11.5103 8.22325"
+            stroke="white"
+            stroke-width="2.97588"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      )}
     </button>
-  )
+  );
 };
 
 export default CustomRightArrow;
